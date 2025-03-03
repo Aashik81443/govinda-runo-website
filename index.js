82e85cf46e0b8204d0263d917a5876897c0321f7
Govinda sharma <ss.dev.454@gmail.com>
2025-03-03 23:14:04 +0545

    create hamburger menu

---
+++ index.js
@@ -0,0 +1,14 @@
+const hamB =document.querySelector("#hamBurger");
+const hamClose =document.querySelector("#hamBurger-close");
+const nav =document.querySelector("#navBar");
+
+hamB.addEventListener('click',()=>{
+nav.classList.add('active');
+})
+
+
+hamClose.addEventListener('click',()=>{
+nav.classList.remove('active');
+})
+
+
